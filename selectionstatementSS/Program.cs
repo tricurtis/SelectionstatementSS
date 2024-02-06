dbd3e1798e594a50628c4854e82e10b6cc317897
Trigail Curtis <trigailcurtisabm@gmail.com>
2024-02-05 18:01:34 -0600

    Updated SelectionStatementSS

---
+++ selectionstatementSS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace selectionstatementSS
+{
+    internal class Program
+    {
+        private static int favNumber;
+
+        static void Main(string[] args)
+        {
+            //Number Guess Game
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+            Console.WriteLine("Pick a number from 1 -1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+}
+        }
+    }
